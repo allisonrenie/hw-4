d5d95071d491f5d5462b631fc71b6f276ad9cca9
allisonrenie <allisonrenie@gmail.com>
2021-05-03 16:45:55 -0700

    list one works now

--- avg_of_list.py
+++ avg_of_list.py
@@ -1,14 +1,23 @@
-def random_list()
-  return
+import random
 
-def list_average(list)
-  return
+def random_list():
+    how_long = random.randint(0,20)
+    list = []
+    while(how_long > 0):
+        list.append(random.randint(0,100))
+        how_long = how_long - 1
+    return list
 
-def print_list(list)
-
+def list_average(list):
+    total = 0
+    for x in list:
+        total += x
+    avg = total/len(list)
+    return avg
 
 my_list = random_list()
 print("List: ")
-print_list(my_list)
+for x in my_list:
+    print(x)
 avg = list_average(my_list)
 print("Average:" + str(avg))
